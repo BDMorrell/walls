ea847882a3b429ba6858fc7905a75ad2bfe43e30
BDMorrell <BDMorrell@gmail.com>
2018-12-29 18:17:44 -0700

    Normalize

--- outputTest.c
+++ outputTest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "maze-defs.h"
 
 // Low level fucntion, and hasn't been tested with edge cases.
 void *mempattern(void * destination, size_t destinationSize, void * source, size_t sourceSize)
@@ -25,12 +26,16 @@ char *generatePatternString(size_t requestedSize, char *source, size_t sourceSiz
   return ret;
 }
 
+char *createMazeTemplate(size2d desiredSize) {
+  char *str = malloc(computeArea(desiredSize))
+}
+
 int main()
 {
   int mazeWidth = 9;
   int mazeHeight = 4;
-  char* hLine = generatePatternString(mazeWidth*2-1, "+-", 2); // 2 excludes '\0'
-  char* vLine = generatePatternString(mazeWidth*2-1, "| ", 2); // 2 excludes '\0'
+  char *hLine = generatePatternString(mazeWidth*2-1, "+-", 2); // 2 excludes '\0'
+  char *vLine = generatePatternString(mazeWidth*2-1, "| ", 2); // 2 excludes '\0'
   int y;
   for (y = 0; y < mazeHeight; y++) {
     printf("%s\n%s\n", hLine, vLine);
