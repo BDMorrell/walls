c1eb24da6ae9d6dd50c9b93345c5391650018994
BDMorrell <BDMorrell@gmail.com>
2018-12-29 20:44:06 -0700

    Add main.c, first for testing
    It's now time to debug this!

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "maze-defs.h"
+
+int main()
+{
+  size2d sizeRequest;
+  sizeRequest.w = 9;
+  sizeRequest.h = 5;
+  maze *myMaze = allocateMaze();
+  completeMaze(myMaze);
+  printMaze(myMaze, stdout);
+  deallocateMaze(myMaze);
+  return 0;
+}
