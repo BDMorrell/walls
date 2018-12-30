7e82668a67cab9d105e32ad1d6f3eff5a8771f6d
BDMorrell <BDMorrell@gmail.com>
2018-12-29 18:52:28 -0700

    Initalize walls for mazes

--- maze-allocation.c
+++ maze-allocation.c
@@ -10,12 +10,56 @@ maze *allocateMaze(size2d requestedSize)
   clearMazeCells(newMaze);
   newMaze->num_walls = calculateWallCount(newMaze->size);
   newMaze->walls = malloc(sizeof(wall) * newMaze->num_walls);
-  //TODO: link walls to cells
+
+  initializeWalls(newMaze);
 }
 
 int calculateWallCount(size2d size)
 {
   return ((size.w)-1)*size.h + ((size.h)-1)*size.w;
+}
+
+void initializeWalls(maze *m)
+{
+  wall *it = m->walls; // iterator
+  int row;
+  for (row = 0; row < m->size.h; row++) {
+    int col;
+    // link right
+    for (col = 0; col < m->size.w-1; col++) { // right-most doesn't have a right
+      initWall(it++, getCell(m, col, row), getCell(m, col + 1, row));
+    }
+    // link down
+    if (row != m->size.h - 1) { // if we aren't in the bottom row
+      for (col = 0; col < m->size.w; col++) {
+        initWall(it++, getCell(m, col, row), getCell(m, col, row + 1));
+      }
+    }
+  }
+}
+
+void initWall(wall &w, cell *a, cell *b)
+{
+  w.active = true;
+  w.sides[0] = a;
+  w.sides[1] = b;
+}
+
+int getOffset(size2d s, pos2d p)
+{
+  if (p.x >= s.w || p.x < 0 || p.y >= s.h || p.y < 0) {
+    return -1; //error
+  } // implicit else
+  return ((p.y * s.w) + p.x);
+}
+
+cell *getCell(maze *m, pos2d position)
+{
+  int offset = getOffset(m->size, position);
+  if (offset == -1) {
+    return NULL:
+  } // implicit else
+  return m->cells[offset];
 }
 
 void dealocateMaze(maze *m)
--- maze-defs.h
+++ maze-defs.h
@@ -5,6 +5,7 @@ typedef struct size2d {
   size_t w, h;
 } size2d;
 
+// TODO: change to int2d
 typedef struct pos2d {
   int x, y;
 } pos2d;
@@ -15,7 +16,7 @@ typedef struct cell {
 
 typedef struct wall {
   struct cell sides[2];
-  struct pos2d pos;
+  struct pos2d pos; // This may not be needed
   bool active;
 } wall;
 
@@ -23,13 +24,17 @@ typedef struct maze {
   struct size2d size;
   size_t num_cells, num_walls;
   struct cell *cells;
-  struct walls *walls;
+  struct wall *walls;
 } maze;
 
 inline size_t computeArea(size2d *point)
 {
   return point->w * point->h;
 }
+
+// Assumptions:
+// We're going to be drawing a rectangular grid maze.
+// It's going to be drawn in quadrant 4; left to right then top to bottom.
 
 //maze-operations.c
 cell *getParent(cell *c);
@@ -40,7 +45,10 @@ void clearMazeCells(maze *m);
 maze *allocateMaze(size2d requestedSize);
 void dealocateMaze(maze *m);
 int calculateWallCount(size2d size);
+void initializeWalls(maze *m);
+void initWall(wall &w, cell *a, cell *b);
+int getOffset(size2d s, pos2d p);
+cell *getCell(maze *m, pos2d position);
 
-//TODO: initalize walls
 //TODO: draw maze
 //TODO: make and draw maze
